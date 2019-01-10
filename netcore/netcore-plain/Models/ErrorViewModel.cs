f4a2ce22dd48c119366668d2bb017fbcd767166d
Josh Rolstad <jorolsta@microsoft.com>
2019-01-10 15:29:27 -0800

    Adding default project

---
+++ netcore/netcore-plain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netcore_plain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
